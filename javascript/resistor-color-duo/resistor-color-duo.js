7f90dd4c0139308003f8553c73cd6c250ffb391a
urza <efarrar@pdx.edu>
2019-12-15 19:32:35 -0800

    Final solution after mentor advice.

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -2,19 +2,9 @@
 // This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-
-export const value = (list) => {  
-  let result;
-
-  if(list.length === 1)
-    result = colorCode(list[0]);
-  else 
-    result = colorCode(list[0]) * 10 + colorCode(list[1]);
-
-  return result;
-};
-
-export const colorCode = (color) => COLORS.indexOf(color);
+export const value = ([colorOne, colorTwo]) => {
+  return COLORS.indexOf(colorOne) * 10 + COLORS.indexOf(colorTwo); 
+}
 
 export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
 
